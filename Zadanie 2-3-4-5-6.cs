a566989178df5c97cde8956913373e8f9245cfcf
najka251 <55061808+najka251@users.noreply.github.com>
2021-11-07 18:19:54 +0100

    Zajęcia 4

---
+++ Zadanie 2-3-4-5-6.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KodZadanie2i3 : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    private float force = 3.0f;
+    public float sensitivity = 10f;
+    private float rotY = 0.0f;
+
+    private void Start()
+    {
+        // zakładamy, że komponent CharacterController jest już podpięty pod obiekt
+        controller = GetComponent<CharacterController>();
+    }
+
+
+
+    void Update()
+    {
+        // wyciągamy wartości, aby możliwe było ich efektywniejsze wykorzystanie w funkcji
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        // zmieniamy sposób poruszania się postaci
+        // Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // transform.right odpowiada za ruch wzdłuż osi x (pamiętajmy, że wartości będą zarówno dodatnie
+        // jak i ujemne, a punkt (0,0) jest na środku ekranu) a transform.forward za ruch wzdłóż osi z.
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // to już nam potrzebne nie będzie
+        //if (move != Vector3.zero)
+        //{
+        //    gameObject.transform.forward = move;
+        //}
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            // wzór na siłę 
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        // prędkość swobodnego opadania zgodnie ze wzorem y = (1/2 * g) * t-kwadrat 
+        // okazuje się, że jest to zbyt wolne opadanie, więc zastosowano g * t-kwadrat
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        //Ograniczenie obrotu kamery do -90 i 90 góra/dół
+        var c = Camera.main.transform;
+        rotY -= Input.GetAxis("Mouse Y") * sensitivity;
+        rotY = Mathf.Clamp(rotY, -90f, 90f);
+        c.transform.eulerAngles = new Vector3(rotY, 0, 0);
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.name == "plytanaciskowa")
+        {
+            playerVelocity.y = force * Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            playerVelocity.y += gravityValue * Time.deltaTime;
+        }
+        if (hit.gameObject.CompareTag("Przeszkoda"))
+        {
+            Debug.Log("Player zderzył się z przeszkoda.");
+        }
+    }
+}
---
+++ Zadanie1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Listing1 : MonoBehaviour
+{
+    List<Vector3> positions = new List<Vector3>();
+    public float delay = 3.0f;
+    public int ile = 5;
+    int objectCounter = 0;
+    // obiekt do generowania
+    public GameObject block;
+    public Material[] kolory = new Material[5];
+
+    void Start()
+    {
+        MeshRenderer plansza = GetComponent<MeshRenderer>();
+        float minX = plansza.bounds.min.x;
+        float maxX = plansza.bounds.max.x;
+        float minZ = plansza.bounds.min.z;
+        float maxZ = plansza.bounds.max.z;
+
+        // w momecie uruchomienia generuje 10 kostek w losowych miejscach
+        List<int> pozycje_x = new List<int>(Enumerable.Range((int)minX, (int)maxX).OrderBy(x => Guid.NewGuid()).Take(ile));
+        List<int> pozycje_z = new List<int>(Enumerable.Range((int)minZ, (int)maxZ).OrderBy(x => Guid.NewGuid()).Take(ile));
+
+        for (int i = 0; i < ile; i++)
+        {
+            this.positions.Add(new Vector3(pozycje_x[i], 0, pozycje_z[i]));
+        }
+        foreach (Vector3 elem in positions)
+        {
+            Debug.Log(elem);
+        }
+        // uruchamiamy coroutine
+        StartCoroutine(GenerujObiekt());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerujObiekt()
+    {
+        Debug.Log("wywołano coroutine");
+        foreach (Vector3 pos in positions)
+        {
+            GameObject obiekt = Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+            obiekt.GetComponent<MeshRenderer>().material = this.kolory[Random.Range(0, this.kolory.Length)];
+            yield return new WaitForSeconds(this.delay);
+        }
+        // zatrzymujemy coroutine
+        StopCoroutine(GenerujObiekt());
+    }
+}
